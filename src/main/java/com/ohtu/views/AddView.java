455f52568ed6f7ef4f1eef4ac8fa436bd18e522c
leppaott <leppanen.otto@gmail.com>
2017-03-31 19:11:11 +0300

    vaadin test

---
+++ src/main/java/com/ohtu/views/AddView.java
@@ -0,0 +1,32 @@
+package com.ohtu.views;
+
+import com.ohtu.models.ReferenceForm;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+@SuppressWarnings("serial")
+public class AddView extends VerticalLayout implements View {
+
+	public AddView(Navigator navigator) {
+		setSizeFull();
+
+		Button button = new Button("Go Back", e -> navigator.navigateTo(""));
+		ReferenceForm form = new ReferenceForm();
+
+		button.setStyleName(ValoTheme.BUTTON_PRIMARY);
+
+		VerticalLayout main = new VerticalLayout(form, button);
+
+		addComponents(main);
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ src/main/java/com/ohtu/views/MainView.java
@@ -0,0 +1,43 @@
+package com.ohtu.views;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.ohtu.app.Main;
+import com.ohtu.models.Reference;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class MainView extends VerticalLayout implements View {
+	private List<Reference> refs = Arrays.asList(new Reference("title1", "author", 1986),
+			new Reference("title2", "author4", 1999));
+
+	public MainView(Navigator navigator) {
+		Button button = new Button("Add references", event -> navigator.navigateTo("addRefs"));
+		Grid<Reference> grid = new Grid<>();
+
+		HorizontalLayout main = new HorizontalLayout(grid, button);
+		main.setSizeFull();
+		grid.setSizeFull();
+		main.setExpandRatio(grid, 1);
+
+		grid.setItems(refs);
+		grid.addColumn(Reference::getTitle).setCaption("Title");
+		grid.addColumn(Reference::getAuthor).setCaption("Author");
+		grid.addColumn(Reference::getYear).setCaption("Year");
+
+		addComponents(main);
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+}
