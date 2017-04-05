103d313a7311a81a2c6bada2812d7edb4c9786b2
Anni Puurunen <puurunen.anni@gmail.com>
2017-04-05 23:20:54 +0300

    article reference test

--- src/main/java/com/ohtu/models/Reference.java
+++ src/main/java/com/ohtu/models/Reference.java
@@ -23,6 +23,18 @@ public class Reference {
 	public int getYear() {
 		return year;
 	}
+        
+        public void setTitle(String title) {
+            this.title = title;
+        }
+        
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+        
+        public void setYear(int year) {
+            this.year = year;
+        }
 	
 	@Override
 	public String toString() {
