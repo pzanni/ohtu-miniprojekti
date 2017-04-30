12bdb303ab31b37da6bfc365dd80aeee3bca0a27
samuli.rouvinen <samuli.rouvinen@cs.helsinki.fi>
2017-04-30 19:52:23 +0300

    Lisätty poistetut testit takaisin

---
+++ src/test/java/ohtu/stubs/AddViewStub.java
@@ -0,0 +1,24 @@
+package ohtu.stubs;
+
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+
+import ohtu.views.AddView;
+
+@SuppressWarnings("serial")
+public class AddViewStub implements AddView {
+	@Override
+	public void enter(ViewChangeEvent event) {
+	}
+
+	@Override
+	public void setListener(AddViewListener listener) {
+	}
+
+	@Override
+	public void success() {
+	}
+
+	@Override
+	public void failure() {
+	}
+}
